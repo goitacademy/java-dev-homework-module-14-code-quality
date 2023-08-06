8e1bdcc7a0cc5f05015a765f373ca82087f1074b
smagles <lesialist@icloud.com>
2023-08-06 11:26:20 +0200

    "Remove unnecessary Scanner object creation"

--- src/main/java/tictactoe/Player.java
+++ src/main/java/tictactoe/Player.java
@@ -37,4 +37,8 @@ public class Player {
             }
         }
     }
+
+    public void closeScanner() {
+        scanner.close();
+    }
 }
--- src/main/java/tictactoe/TicTacToeGame.java
+++ src/main/java/tictactoe/TicTacToeGame.java
@@ -1,17 +1,16 @@
 package tictactoe;
 
-import java.util.Scanner;
 
 public class TicTacToeGame {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         GameBoard gameBoard = new GameBoard();
         Player player = new Player(GameBoard.PLAYER_SYMBOL);
         Computer computer = new Computer(GameBoard.COMPUTER_SYMBOL);
 
         gameBoard.displayStartMessage();
         playGame(gameBoard, player, computer);
-        scanner.close();
+
     }
 
     public static void playGame(GameBoard gameBoard, Player player, Computer computer) {
@@ -38,6 +37,8 @@ public class TicTacToeGame {
 
             int computerMove = computer.getComputerMove(gameBoard);
             gameBoard.makeMove(computerMove, computer.getSymbol());
+
+            player.closeScanner();
         }
     }
 }
